dd6f712077b5f05bced7b77c7eba07a7441a4e50
h671420 <671420@stud.hvl.no>
2024-11-14 23:10:44 +0100

    asd

--- Javascript/TeamUp/Bruker/UIElementer/ExpandedState.js
+++ Javascript/TeamUp/Bruker/UIElementer/ExpandedState.js
@@ -1,4 +1,5 @@
 import UIElement from "https://kaffannen.github.io/Solution/Javascript/EzUI/DeveloperClasses/UIElement.js";
+import Student from "https://kaffannen.github.io/Solution/Javascript/TeamUp/Bruker/Student.js";
 export default class ExpandedState extends UIElement
 {
     constructor(nexus) {
