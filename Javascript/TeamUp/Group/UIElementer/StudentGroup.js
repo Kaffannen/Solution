d354db29fff05fb52534fee29b44ac6abe4885c2
h671420 <671420@stud.hvl.no>
2024-11-14 22:37:24 +0100

    asd

--- Javascript/TeamUp/Group/UIElementer/StudentGroup.js
+++ Javascript/TeamUp/Group/UIElementer/StudentGroup.js
@@ -1,4 +1,5 @@
 import UIElement from "https://kaffannen.github.io/Solution/Javascript/EzUI/DeveloperClasses/UIElement.js";
+import AssignmentGroupMember from "https://kaffannen.github.io/Solution/Javascript/TeamUp/GroupMember/UIElements.js";
 export default class StudentGroup extends UIElement {
 
     constructor(nexus) {
