6189c686910419ebfb164d1a543adb09ec89d2e4
h671420 <671420@stud.hvl.no>
2024-11-15 15:18:01 +0100

    asd

--- Javascript/TeamUp/DevMain.js
+++ Javascript/TeamUp/DevMain.js
@@ -1,10 +1,10 @@
 let program;
 document.addEventListener("DOMContentLoaded", async function () {
     let api = new API()
-        .setCanvasApi(new CanvasAPIMock())
-        .setMsgBroker(new MsgBrokerMock())
-        .setPersistence(new PersistenceMock())
-    window.program = new BasicSolution(new API())
+        .setCanvasApi(CanvasAPIMock)
+        .setMsgBroker(MsgBrokerMock)
+        .setPersistence(PersistenceMock)
+    window.program = new BasicSolution(api)
     program = window.program;
     await program.defineUIElements();
     program.getFavourite().setState(Student.STATES.EXPANDED);
