a36a627d0396ac4d04dc310404f601f411a1e647
h671420 <671420@stud.hvl.no>
2024-11-15 15:46:01 +0100

    asd

--- Javascript/TM_script.js
+++ Javascript/TM_script.js
@@ -4,20 +4,21 @@
 // @version      2024-11-14
 // @description  try to take over the world!
 // @author       You
-// @match        https://hvl.instructure.com/courses/\d+/assignments/\d+
+// @match        https://hvl.instructure.com/courses/*/assignments/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
 
 window.addEventListener("load", function () {
     setTimeout(function () {
+        // Select all span elements with data-testid="title"
         const spanElements = document.querySelectorAll('span[data-testid="title"]');
         let targetSpan = null;
+
+        // Loop through the span elements and find the one with data-testid="title"
         for (let span of spanElements) {
-            if (span.textContent.trim() === "Samarbeidsavtale") {
-                targetSpan = span;
-                break;
-            }
+            targetSpan = span;
+            break; // Break after finding the first match
         }
 
         if (targetSpan) {
@@ -25,8 +26,10 @@ window.addEventListener("load", function () {
             ezAnchor.id = "EzAnchor";
             targetSpan.insertAdjacentElement("afterend", ezAnchor);
         } else {
-            console.error('No span with the content "Samarbeidsavtale" and data-testid="title" found!');
+            console.error('No span with data-testid="title" found!');
         }
+
+        // Load the external script
         const script = document.createElement('script');
         script.src = 'https://kaffannen.github.io/Solution/Javascript/TeamUpBundle.js';
         script.type = 'module';
