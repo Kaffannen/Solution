0eaaccc8233742e60520b8cc64f9ea1e150278e3
h671420 <671420@stud.hvl.no>
2024-11-20 21:14:41 +0100

    Update script URLs to use bundled versions in TM_script.js

--- Javascript/TM_script.js
+++ Javascript/TM_script.js
@@ -14,11 +14,11 @@ const override = "teacher"
 
 let scriptUrl;
 if (override === "teacher")
-    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/TeamUp/TMTeacherProtoMain.js';
+    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/bundle_TMTeacherProtoMain.js';
 else if (override === "student")
-    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/TeamUp/TMStudentProtoMain.js';
+    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/bundle_TMStudentProtoMain.js';
 else
-    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/TeamUp/TMMain.js';
+    scriptUrl = 'https://kaffannen.github.io/Solution/Javascript/bundle_TMMain.js';
 
 window.addEventListener("load", function () {
     setTimeout(function () {
