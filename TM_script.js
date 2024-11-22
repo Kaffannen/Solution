9a75ba1cc562e75e39f9119b178a891b9ab90301
h671420 <671420@stud.hvl.no>
2024-11-22 19:16:59 +0100

    Update instructions and formatting for override selection in TM_script.js

--- TM_script.js
+++ TM_script.js
@@ -10,18 +10,26 @@
 // ==/UserScript==
 
 /*
-    VELG EN OVERRIDE (kommenter bort de andre alternativene med '//')
+    1: Marker all tekst på denne siden (CTRL + A) 
+    2: Åpne opp Tampermonkey og trykk på "Create a new script..."
+    3: Marker all tekst i det nye scriptet (CTRL + A) og lim inn denne koden 
+    4: Trykk på "File" -> "Save" (CTRL + S)
+    5: Velg en 'override', som vist under
+    6: Naviger til en oppgave i et valgfritt kurs på Canvas 
+    
 
     Gå inn på en valgfri oppgave til et valgfritt kurs, og se hva som dukker opp!
 
 */
 
-//const override = "student"
-const override = "underviser"
-//const override = "ingen"
+/* VELG EN OVERRIDE (kommenter bort de andre alternativene med '//') */
+
+    //const override = "student"
+    const override = "underviser"
+    //const override = "ingen"
 
 
-/* */
+
 
 
 
