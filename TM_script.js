723458ae5e5353ba39d04c889eb4dc11d6610275
h671420 <671420@stud.hvl.no>
2024-11-22 16:33:02 +0100

    Update script URLs in TM_script.js to point to the Solution directory

--- TM_script.js
+++ TM_script.js
@@ -14,11 +14,11 @@ const override = "teacher"
 
 let scriptUrl;
 if (override === "teacher")
-    scriptUrl = 'https://kaffannen.github.io/Compiles/bundle_TMTeacherProtoMain.js';
+    scriptUrl = 'https://kaffannen.github.io/Solution/Compiles/bundle_TMTeacherProtoMain.js';
 else if (override === "student")
-    scriptUrl = 'https://kaffannen.github.io/Compiles/bundle_TMStudentProtoMain.js';
+    scriptUrl = 'https://kaffannen.github.io/Solution/Compiles/bundle_TMStudentProtoMain.js';
 else
-    scriptUrl = 'https://kaffannen.github.io/Compiles/bundle_TMMain.js'; 
+    scriptUrl = 'https://kaffannen.github.io/Solution/Compiles/bundle_TMMain.js'; 
 
 window.addEventListener("load", function () {
     setTimeout(function () {
