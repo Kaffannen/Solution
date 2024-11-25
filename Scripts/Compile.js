0efe874c327d0128a8472163e47bd4c146b5dd0e
h671420 <671420@stud.hvl.no>
2024-11-25 07:57:50 +0100

    Enable logging of fileset entries in createLines function for better debugging

--- Scripts/Compile.js
+++ Scripts/Compile.js
@@ -114,7 +114,7 @@ function createLines(mainFileObjects, allFileObjects) {
     function createLine(fileObject, fileObjects) {
         let fileSet = createFileSet(new Set().add(fileObject), fileObjects);
         console.log(`fileset belonging to ${fileObject.path}: has ${fileSet.size} files.`);
-        //console.log("fileset entries:", JSON.stringify(Array.from(fileSet), null, 2));
+        console.log("fileset entries:", JSON.stringify(Array.from(fileSet), null, 2));
         let arr = sortTopologically(fileSet);
         console.log(`line belonging to ${fileObject.path}: has ${arr.length} files.`);
         console.log("line entries:", JSON.stringify(arr, null, 2));
